324cdb24700a45074eae2be3779d3b876ba5fe6a
Alberto Zuccari <albertozuccari8@gmail.com>
2023-11-09 15:15:22 +0100

    lab5

--- Materiale lab5/exercise5.py
+++ Materiale lab5/exercise5.py
@@ -31,30 +31,32 @@ import matplotlib.pyplot as plt
 # Exercise 1
 # Function approssimazioni successive
 def succ_app(f, g, tolf, tolx, maxit, xTrue, x0=0):
-  i=0
-  err=np.zeros(maxit+1, dtype=np.float64)
-  err[0]=tolx+1
-  vecErrore=np.zeros(maxit+1, dtype=np.float64)
-  vecErrore[0] = np.abs(xTrue - x0)
-  x=x0
+    i = 0
+    err = np.zeros(maxit + 1, dtype=np.float64)
+    err[0] = tolx + 1
+    vecErrore = np.zeros(maxit + 1, dtype=np.float64)
+    vecErrore[0] = np.abs(xTrue - x0)
+    x = x0
 
-  while (i<maxit or (np.abs(f(x))>tolf) or err[i]>tolx): # scarto assoluto tra iterati
-    x_new= g(x)
-    err[i+1]= np.abs(x_new - x)
-    vecErrore[i+1]= np.abs(xTrue - x_new)
-    i=i+1
-    x=x_new
-  err=err[0:i]
-  vecErrore = vecErrore[0:i]
-  return (x, i, err, vecErrore) 
+    while (i < maxit and (np.abs(f(x)) > tolf) and err[i] > tolx):  # corretto l'operatore logico 'or' in 'and'
+        x_new = g(x)
+        i = i + 1
+        if i < maxit:
+            err[i] = np.abs(x_new - x)
+            vecErrore[i] = np.abs(xTrue - x_new)
+        x = x_new
+
+    err = err[0:i]
+    vecErrore = vecErrore[0:i]
+    return (x, i, err, vecErrore)
 
 def newton(f, df, tolf, tolx, maxit, xTrue, x0=0):
-    g = lambda x: ...
+    g = lambda x: x - f(x) / df(x)
     (x, i, err, vecErrore) = succ_app(f, g, tolf, tolx, maxit, xTrue, x0)
     return (x, i, err, vecErrore)
 
 f = lambda x: np.exp(x)-x**2
-df = lambda x: ...
+df = lambda x: np.exp(x) - 2 * x
 g1 = lambda x: x-f(x)*np.exp(x/2)
 g2 = lambda x: x-f(x)*np.exp(-x/2)
 
@@ -108,96 +110,96 @@ plt.grid()
 plt.show()
 
 
-# Esercizio 2.1
-f = lambda x: x**3+4*x*np.cos(x)-2
-df = lambda x: ...
-g1 = lambda x: (2-x**3)/(4*np.cos(x))
+# # Esercizio 2.1
+# f = lambda x: x**3+4*x*np.cos(x)-2
+# df = lambda x: ...
+# g1 = lambda x: (2-x**3)/(4*np.cos(x))
 
-xTrue = 0.536839
-fTrue = f(xTrue)
-print('fTrue = ', fTrue)
+# xTrue = 0.536839
+# fTrue = f(xTrue)
+# print('fTrue = ', fTrue)
 
-xplot = np.linspace(0, 2)
-fplot = f(xplot)
+# xplot = np.linspace(0, 2)
+# fplot = f(xplot)
 
-plt.plot(xplot,fplot)
-plt.plot(xTrue,fTrue, 'or', label='True')
+# plt.plot(xplot,fplot)
+# plt.plot(xTrue,fTrue, 'or', label='True')
 
-tolx= 10**(-10)
-tolf = 10**(-6)
-maxit=100
-x0= ...
+# tolx= 10**(-10)
+# tolf = 10**(-6)
+# maxit=100
+# x0= ...
 
-[sol_g1, iter_g1, err_g1, vecErrore_g1]=succ_app(f, g1, tolf, tolx, maxit, xTrue, x0)
-print('Metodo approssimazioni successive g1 \n x =',sol_g1,'\n iter_new=', iter_g1)
+# [sol_g1, iter_g1, err_g1, vecErrore_g1]=succ_app(f, g1, tolf, tolx, maxit, xTrue, x0)
+# print('Metodo approssimazioni successive g1 \n x =',sol_g1,'\n iter_new=', iter_g1)
 
-plt.plot(sol_g1,f(sol_g1), '*', label='g1')
+# plt.plot(sol_g1,f(sol_g1), '*', label='g1')
 
-[sol_newton, iter_newton, err_newton, vecErrore_newton]=newton(f, df, tolf, tolx, maxit, xTrue, x0)
-print('Metodo Newton \n x =',sol_newton,'\n iter_new=', iter_newton)
+# [sol_newton, iter_newton, err_newton, vecErrore_newton]=newton(f, df, tolf, tolx, maxit, xTrue, x0)
+# print('Metodo Newton \n x =',sol_newton,'\n iter_new=', iter_newton)
 
-plt.plot(sol_newton,f(sol_newton), '+b', label='Newton')
-plt.grid()
-plt.legend()
-plt.show()
+# plt.plot(sol_newton,f(sol_newton), '+b', label='Newton')
+# plt.grid()
+# plt.legend()
+# plt.show()
 
-# GRAFICO Errore vs Iterazioni
+# # GRAFICO Errore vs Iterazioni
 
-# g1
-plt.plot(vecErrore_g1, '.-', color='blue')
-# Newton
-plt.plot(vecErrore_newton, '.-', color='red')
+# # g1
+# plt.plot(vecErrore_g1, '.-', color='blue')
+# # Newton
+# plt.plot(vecErrore_newton, '.-', color='red')
 
-plt.legend( ("g1", "newton"))
-plt.xlabel('iter')
-plt.ylabel('errore')
-plt.title('Errore vs Iterazioni')
-plt.grid()
-plt.show()
+# plt.legend( ("g1", "newton"))
+# plt.xlabel('iter')
+# plt.ylabel('errore')
+# plt.title('Errore vs Iterazioni')
+# plt.grid()
+# plt.show()
 
-# Esercizio 2.2
-f = lambda x: x-x**(1/3)-2
-df = lambda x: ...
-g1 = lambda x: x**(1/3)+2
+# # Esercizio 2.2
+# f = lambda x: x-x**(1/3)-2
+# df = lambda x: ...
+# g1 = lambda x: x**(1/3)+2
 
-xTrue = 3.5213
-fTrue = f(xTrue)
-print('fTrue = ', fTrue)
+# xTrue = 3.5213
+# fTrue = f(xTrue)
+# print('fTrue = ', fTrue)
 
-xplot = np.linspace(3, 5)
-fplot = f(xplot)
+# xplot = np.linspace(3, 5)
+# fplot = f(xplot)
 
-plt.plot(xplot,fplot)
-plt.plot(xTrue,fTrue, '^r', label='True')
+# plt.plot(xplot,fplot)
+# plt.plot(xTrue,fTrue, '^r', label='True')
 
-tolx= 10**(-10)
-tolf = 10**(-6)
-maxit=100
-x0= 3
+# tolx= 10**(-10)
+# tolf = 10**(-6)
+# maxit=100
+# x0= 3
 
-[sol_g1, iter_g1, err_g1, vecErrore_g1]=succ_app(f, g1, tolf, tolx, maxit, xTrue, x0)
-print('Metodo approssimazioni successive g1 \n x =',sol_g1,'\n iter_new=', iter_g1)
+# [sol_g1, iter_g1, err_g1, vecErrore_g1]=succ_app(f, g1, tolf, tolx, maxit, xTrue, x0)
+# print('Metodo approssimazioni successive g1 \n x =',sol_g1,'\n iter_new=', iter_g1)
 
-plt.plot(sol_g1,f(sol_g1), 'o', label='g1')
+# plt.plot(sol_g1,f(sol_g1), 'o', label='g1')
 
-[sol_newton, iter_newton, err_newton, vecErrore_newton]=newton(f, df, tolf, tolx, maxit, xTrue, x0)
-print('Metodo Newton \n x =',sol_newton,'\n iter_new=', iter_newton)
+# [sol_newton, iter_newton, err_newton, vecErrore_newton]=newton(f, df, tolf, tolx, maxit, xTrue, x0)
+# print('Metodo Newton \n x =',sol_newton,'\n iter_new=', iter_newton)
 
-plt.plot(sol_newton,f(sol_newton), '+b', label='Newton')
-plt.grid()
-plt.legend()
-plt.show()
+# plt.plot(sol_newton,f(sol_newton), '+b', label='Newton')
+# plt.grid()
+# plt.legend()
+# plt.show()
 
-# GRAFICO Errore vs Iterazioni
+# # GRAFICO Errore vs Iterazioni
 
-# g1
-plt.plot(vecErrore_g1, '.-', color='blue')
-# Newton
-plt.plot(vecErrore_newton, '.-', color='red')
+# # g1
+# plt.plot(vecErrore_g1, '.-', color='blue')
+# # Newton
+# plt.plot(vecErrore_newton, '.-', color='red')
 
-plt.legend( ("g1", "newton"))
-plt.xlabel('iter')
-plt.ylabel('errore')
-plt.title('Errore vs Iterazioni')
-plt.grid()
-plt.show()
+# plt.legend( ("g1", "newton"))
+# plt.xlabel('iter')
+# plt.ylabel('errore')
+# plt.title('Errore vs Iterazioni')
+# plt.grid()
+# plt.show()
